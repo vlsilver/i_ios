1b4e9c01824cebc8631f6fe209a9cfb1ddd4c94e
vlsilver <silvervlhorse128@gmail.com>
2021-10-17 14:04:19 +0700

    add Dicee Swift UI App

---
+++ Dicee SwiftUI/Dicee SwiftUI/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  Dicee SwiftUI
+//
+//  Created by niBVL on 17/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+   @State var leftDiceNumber = 1
+   @State var rightDiceNumber = 1
+    var body: some View {
+        ZStack{
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                     DiceView(n:leftDiceNumber)
+                     DiceView(n:rightDiceNumber)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+                .cornerRadius(16.0)
+                
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct DiceView: View {
+    let n: Int
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
+    }
+}
